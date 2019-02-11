1a6a80fbd8cabe9d16409e899022716a2f995138
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2019-02-11 10:15:08 +0700

    Move prototype to examples

--- examples/rust/src/main.rs
+++ examples/rust/src/main.rs
@@ -1,14 +1,21 @@
 mod statechart;
+mod my_system;
 
-use statechart::*;
+use statechart::{Machine};
+
+use my_system::*;
 
 fn main() {
     // let mut traffic_light = Machine::<Light>::new();
     let mut lamp = Machine::<Switch>::new();
     let mut traffic_light = Machine::from(Light::Green);
 
-    traffic_light.on_transition(|state, previous, event| println!("{:?} -> {:?} @ {:?}", previous, state, event));
-    lamp.on_transition(|state, previous, event| println!("{:?} -> {:?} @ {:?}", previous, state, event));
+    traffic_light.on_transition(|state, previous, event| {
+        println!("{:?} -> {:?} @ {:?}", previous, state, event)
+    });
+    lamp.on_transition(|state, previous, event| {
+        println!("{:?} -> {:?} @ {:?}", previous, state, event)
+    });
     traffic_light.on_final(|state, event| println!("{:?} -> <final> @ {:?}", state, event));
 
     // loop {
@@ -16,7 +23,9 @@ fn main() {
     for x in 1..15 {
         if x == 13 {
             traffic_light.send(Event::Poweroff)
-        } else { traffic_light.send(Event::Timer) }
+        } else {
+            traffic_light.send(Event::Timer)
+        }
 
         if x % 4 == 0 {
             lamp.send(Event::Push)
