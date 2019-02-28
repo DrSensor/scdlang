3135141ee394f39c727618d4c020059d9b690a9b
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2019-02-28 09:31:28 +0700

    Add Github Action workflow (#2)
    Experiment new CI pipeline for measuring performance

---
+++ .github/action/summarize-perf/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+
+# -------------------- helper -------------------------
+register() {
+  read json
+  git config --global user.name "$(echo ${json} | jq .author.name)"
+  git config --global user.email "$(echo ${json} | jq .author.email)"
+}
+
+getinfo() {
+  curl $([ -z $GITHUB_TOKEN ] || echo "-H 'Authorization: token ${GITHUB_TOKEN}'") -s https://api.github.com/repos/${GITHUB_REPOSITORY}/git/commits/${GITHUB_SHA} | jq -c .
+}
+# -----------------------------------------------------
+
+getinfo | register
+export PERF_HOME=${PERF_HOME:-${HOME}/.perf}
+
+git fetch -q origin refs/notes/*:refs/notes/*
+python --version
+pipenv --version
+
+# TODO: investigate why it's not working but it works on action/perf
+# ${PERF_SETUP:-$(pipenv install --system)}
+
+if [ -z "$PERF_SETUP" ]; then
+  pipenv install --system
+else
+  ${PERF_SETUP}
+fi
+
+sh -c "$*"
+
+${PERF_TEARDOWN}
