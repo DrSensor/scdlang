46e7898130cddcb82cf92fff569ad458b7f4aa91
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2019-03-28 06:07:08 +0700

    Fix undefined variable 😂
    Maybe I should `just lint` on  pre-commit hooks 🤔

--- scripts/gensample.py
+++ scripts/gensample.py
@@ -13,4 +13,4 @@ for x in range(int(sys.argv[1])):
     current_state = f"A{random(1)}"
     next_state = f"B{random(1)}"
     event = f"C{random(1)}"
-    print(f"{current_state} -> {next_state} {trigger}")
+    print(f"{current_state} -> {next_state}")
