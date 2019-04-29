468c6183812bef594318118976f2901e202b434f
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2019-04-29 23:12:44 +0700

    Inspect failed action on `summarize-perf` image

--- .github/action/summarize-perf/entrypoint.sh
+++ .github/action/summarize-perf/entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 # -------------------- helper -------------------------
 register() {
   read json
+  echo ${json}
+  echo "$(echo ${json} | jq .author.name)"
   git config --global user.name "$(echo ${json} | jq .author.name)"
   git config --global user.email "$(echo ${json} | jq .author.email)"
 }
