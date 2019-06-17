cd08ccb1f9418b2ee128d91641cb4735f4c057f7
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2019-06-18 04:01:09 +0700

    Fine tune the error messages

--- packages/core/src/error/format.rs
+++ packages/core/src/error/format.rs
@@ -3,28 +3,18 @@ use crate::{grammar::*, Scdlang};
 use pest::error::ErrorVariant::{CustomError, ParsingError};
 use std::error;
 
-type Rules = Vec<Rule>;
-
 impl FineTune for PestError {
 	fn tune_variant(mut self) -> Self {
 		use Rule::*;
 		if let ParsingError { positives, negatives } = self.variant {
-			let exclude_positives = [EOI];
-			let mut positives = positives
-				.into_iter()
-				.filter(|rule| exclude_positives.iter().all(|r| r != rule))
-				.collect::<Rules>();
+			let negatives = negatives.excludes(&[EOI, PASCAL_CASE, QUOTED]);
+
+			let mut positives = positives.excludes(&[EOI]);
 			positives = match &positives[..] {
 				[Symbol::at, Name::state] => vec![Symbol::at],
 				[Rule::expression, Symbol::at] => vec![Symbol::at],
 				_ => positives,
 			};
-
-			let exclude_negatives = [EOI, PASCAL_CASE, QUOTED];
-			let negatives = negatives
-				.into_iter()
-				.filter(|rule| exclude_negatives.iter().all(|r| r != rule))
-				.collect::<Rules>();
 
 			if positives.is_empty() && negatives.is_empty() {
 				self.variant = CustomError {
@@ -51,13 +41,24 @@ impl<'t> Scdlang<'t> {
 			Symbol::arrow::right => "->".to_string(),
 			Symbol::arrow::left => "<-".to_string(),
 			Symbol::at => "@".to_string(),
-			Rule::transition => "-> or <-".to_string(),
+			Rule::transition => "-->, <--, ->>, <<-, >->, <-<, or <->".to_string(),
 			_ => format!("{:?}", rule),
 		})
 	}
 }
 
-/// Fine tune syntax error
-pub(super) trait FineTune: error::Error {
+impl Exclusion for Vec<Rule> {
+	fn excludes(self, rules: &[Rule]) -> Self {
+		self.into_iter().filter(|rule| rules.iter().all(|r| r != rule)).collect()
+	}
+}
+
+trait FineTune: error::Error {
+	/// Fine tune syntax error
 	fn tune_variant(self) -> Self;
+}
+
+trait Exclusion: PartialOrd<Vec<Rule>> {
+	/// Excludes several Rule
+	fn excludes(self, rules: &[Rule]) -> Self;
 }
