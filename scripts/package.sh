89f32cbfa5a6e787350781f6308edcec124e243b
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2019-06-30 04:12:59 +0700

    Try workaround on azure pipelines issues
    https://github.com/MicrosoftDocs/vsts-docs/issues/4689

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
-shopt -s globstar # TODO: waiting https://github.com/MicrosoftDocs/vsts-docs/issues/4687 to resolved
+shopt -s extglob
+# shopt -s globstar # TODO: waiting https://github.com/MicrosoftDocs/vsts-docs/issues/4687 to resolved
 
-for cargo_toml in packages/**/Cargo.toml; do
+for cargo_toml in packages/{*,transpiler/*}/Cargo.toml; do
   cargo package --manifest-path $cargo_toml --no-verify $@
 done
