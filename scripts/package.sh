4ea058523417e2b783693e11eec0894a91296cb1
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2019-06-30 02:36:07 +0700

    Disable globstar to support bash 3.1 ⚠️
    Azure pipelines still use Bash 3.1 instead of 5/4 😞

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
-shopt -s extglob
-shopt -s globstar
+# shopt -s extglob
+# shopt -s globstar # azure-pipelines still use bash 3.1 😞
 
-for cargo_toml in packages/**/Cargo.toml; do
+for cargo_toml in packages/{transpiler/*,*}/Cargo.toml; do
   cargo package --manifest-path $cargo_toml --no-verify $@
 done
