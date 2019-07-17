2c6d00022de2ea8b9596d5591ff98c94baf806c1
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2019-07-17 14:33:47 +0700

    Update libc6 and fix linter warning

--- .github/action/perf/wrap-args.py
+++ .github/action/perf/wrap-args.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-import re,sys
+import re
+import sys
 from itertools import zip_longest
 from subprocess import run
 
 assert len(sys.argv) > 1
+
 
 # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
 def grouper(iterable, n, fillvalue=None):
@@ -11,19 +13,27 @@ def grouper(iterable, n, fillvalue=None):
     args = [iter(iterable)] * n
     return zip_longest(*args, fillvalue=fillvalue)
 
+
 def group_command(flatten_commands, commands):
     "Group string by it's command name"
     separator = f"({'|'.join(list_command)})"
-    separated_cmd_params = [s.strip() for s in re.split(separator, flatten_commands)[1:]]
-    return [' '.join(s) for s in grouper(separated_cmd_params, 2)]
+    separated_cmd_params = [
+        s.strip() for s in re.split(separator, flatten_commands)[1:]
+    ]
+    return [" ".join(s) for s in grouper(separated_cmd_params, 2)]
 
-cargo_list = run(['cargo', '--list'], capture_output=True)
-stdout = cargo_list.stdout.decode('utf-8')
 
-command_help = stdout.split('\n')[1:-1]             # remove endline and title string
-list_command =[s.split(maxsplit=1)[0] for s in command_help]  # remove help description
+cargo_list = run(["cargo", "--list"], capture_output=True)
+stdout = cargo_list.stdout.decode("utf-8")
 
-grouped = group_command(' '.join(sys.argv[1:]), list_command)
+command_help = stdout.split("\n")[1:-1]  # remove endline and title string
+list_command = [s.split(maxsplit=1)[0] for s in command_help]  # remove help description
 
-prefix_command = lambda f: '"{prefix} {}"'.format(f'" "{f} '.join(grouped), prefix=f)
-print(prefix_command('cargo'))
+grouped = group_command(" ".join(sys.argv[1:]), list_command)
+
+
+def prefix_command(f):
+    '"{prefix} {}"'.format(f'" "{f} '.join(grouped), prefix=f)
+
+
+print(prefix_command("cargo"))
